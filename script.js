b96b0d5d71ab5adc530c32f431fde0a7ac3bfe33
dahamm <Muhammad.Dahamshi@emc.com>
2023-05-16 15:13:15 +0300

    update

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function updateCount() {
-    const current = Number(1207);
-
     const timezone = 'Asia/Jerusalem';
     const now = new Date();
     const start = new Date('2023-05-16');
     const nowInJerusalem = new Date(now.toLocaleString('en-US', {timeZone: timezone}));
     const startInJerusalem = new Date(start.toLocaleString('en-US', {timeZone: timezone}));
     const diff = Math.floor((nowInJerusalem - startInJerusalem) / (1000 * 60 * 60 * 24));
-    document.getElementById('count').textContent = current + diff;
+    document.getElementById('count').textContent = Number(1207 + diff).toString();
 }
 
 updateCount();
